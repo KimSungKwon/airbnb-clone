f0b92e0bd6cba2d6bc3bb277da1adf543c70bae9
KimSungKwon <sungkwonn@naver.com>
2023-04-30 22:27:05 +0900

    create get User Session

---
+++ airbnb-video/app/actions/getCurrentUser.ts
@@ -0,0 +1,36 @@
+import { getServerSession } from "next-auth/next";
+
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from '@/app/libs/prismadb'
+
+/*
+*   현재 로그인한 유저 세션을 받아옴
+*/
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
+    // user 세션이 비어있으면
+    if (!session?.user?.email) {
+      return null;
+    }
+    // email을 통해 현재 유저를 얻음
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string
+      }
+    });
+    // email과 동일한게 없으면
+    if (!currentUser) {
+      return null;
+    }
+
+    return currentUser;
+  } catch (error: any) {
+    return null;
+  }
+}
