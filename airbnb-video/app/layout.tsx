15c5e2c11c1c6464a8a0255c2c33c445fed274d8
KimSungKwon <sungkwonn@naver.com>
2023-04-30 22:41:56 +0900

    add get User Session and Log out

--- airbnb-video/app/layout.tsx
+++ airbnb-video/app/layout.tsx
@@ -5,6 +5,8 @@ import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const font = Nunito({ // add font
   subsets: ["latin"],
@@ -15,18 +17,21 @@ export const metadata = {
   description: 'Airbnb clone',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser(); // 로그인한 유저 세션 받아옴. 없으면 null
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body> {/* add font*/}
