ecf05b12f0cc0cfd5a7bd58c847b515962a7de5d
KimSungKwon <sungkwonn@naver.com>
2023-05-04 22:41:30 +0900

    no message

--- airbnb-video/app/components/Container.tsx
+++ airbnb-video/app/components/Container.tsx
@@ -3,7 +3,7 @@
 interface ContainerProps {
   children: React.ReactNode;
 }
-
+// 화면크기에 따른 크기 조절
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return ( 
     <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
