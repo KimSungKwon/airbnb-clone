fc522a64008cc3b63bd6ac78d18a51ee126c60b1
KimSungKwon <sungkwonn@naver.com>
2023-05-04 22:40:57 +0900

    routing

--- airbnb-video/app/components/navbar/Logo.tsx
+++ airbnb-video/app/components/navbar/Logo.tsx
@@ -7,11 +7,13 @@ const Logo = () => {
   const router = useRouter();
   return ( 
     <Image 
+      onClick={() => router.push('/')}
       alt="Logo" 
       className="hidden md:block cursor-pointer" 
       height="100" 
       width="100"
-      src="/images/logo.png" />
+      src="/images/logo.png" 
+    />
   );
 }
  
