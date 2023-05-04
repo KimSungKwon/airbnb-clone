9e9ab24d71c7f28b56a6b72d705afa3817b09145
KimSungKwon <sungkwonn@naver.com>
2023-05-04 17:53:58 +0900

    add User Image

--- airbnb-video/app/components/Avatar.tsx
+++ airbnb-video/app/components/Avatar.tsx
@@ -2,9 +2,19 @@
 
 import Image from "next/image";
 
-const Avatar = () => {
+interface AvartarProps {
+  src: string | null | undefined;
+}
+
+const Avatar: React.FC<AvartarProps> = ({ src }) => {
   return ( 
-    <Image className="rounded-full" height="30" width="30" alt="Avatar" src="/images/placeholder.jpg" />
+    <Image 
+      className="rounded-full" 
+      height="30" 
+      width="30" 
+      alt="Avatar" 
+      src={src || "/images/placeholder.jpg"} 
+    />
    );
 }
  
