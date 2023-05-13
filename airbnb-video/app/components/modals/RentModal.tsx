63b906a32e3d9f958d3e9a20e47d5492a10ed8f0
KimSungKwon <sungkwonn@naver.com>
2023-05-13 15:59:34 +0900

    create Info Step

--- airbnb-video/app/components/modals/RentModal.tsx
+++ airbnb-video/app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
   CATEGORY = 0,
@@ -33,7 +34,7 @@ const RentModal = () => {
       location: null,
       guestCount: 1,
       roomCount: 1,
-      batchroomCount: 1,
+      bathroomCount: 1,
       imageSrc: '',
       price: 1,
       title: '',
@@ -44,6 +45,9 @@ const RentModal = () => {
   // 값들 변수로 저장
   const category = watch('category');
   const location = watch('location');
+  const guestCount = watch('guestCount');
+  const roomCount = watch('roomCount');
+  const bathroomCount = watch('bathroomCount');
 
   // dynamic import로 SSR하지않고, location이 바뀔때마다 import
   const Map = useMemo(() => dynamic(() => import('../Map'), { 
@@ -120,6 +124,39 @@ const RentModal = () => {
           onChange={(value) => setCustomValue('location', value)}
         />
         <Map center={location?.latlng} />
+      </div>
+    )
+  }
+  /*
+  *   INFO Step
+  */
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenities do you have?"
+        />
+        <Counter 
+          title="Guests"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue('guestCount', value)}
+        />
+        <hr />
+        <Counter 
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => setCustomValue('roomCount', value)}
+        />
+        <hr />
+        <Counter 
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+          value={bathroomCount}
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
       </div>
     )
   }
