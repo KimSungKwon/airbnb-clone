7ab6d841fc4be9dc67e486751344007ff7590415
KimSungKwon <sungkwonn@naver.com>
2023-05-14 17:37:14 +0900

    create Description Step

--- airbnb-video/app/components/modals/RentModal.tsx
+++ airbnb-video/app/components/modals/RentModal.tsx
@@ -13,6 +13,7 @@ import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
+import Input from "../inputs/input";
 
 enum STEPS {
   CATEGORY = 0,
@@ -27,7 +28,7 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY); // initial: 0
-
+  const [isLoading, setIsLoading] = useState(false);
   // Listing schema
   const { register, handleSubmit, setValue, watch, formState: { errors }, reset } = useForm<FieldValues>({
     defaultValues: {
@@ -175,6 +176,36 @@ const RentModal = () => {
         <ImageUpload 
           value={imageSrc}
           onChange={(value) => setCustomValue('imageSrc', value)}
+        />
+      </div>
+    )
+  }
+  /*
+  *   DESCRIPTION Step
+  */
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+          title="How would you describe your place?"
+          subtitle="Short and sweet works best!"
+        />
+        <Input
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
         />
       </div>
     )
