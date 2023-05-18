275b93e079fe1941c1399a28b1c4cf6763da5f58
KimSungKwon <sungkwonn@naver.com>
2023-05-18 22:11:41 +0900

    create Listing page

---
+++ airbnb-video/app/actions/getListingById.ts
@@ -0,0 +1,39 @@
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  listingId?: string;
+}
+
+export default async function getListingById(params: IParams) {
+  try {
+    const { listingId } = params;
+
+    // id가 listingId값과 같고, user값이 있는 listing 데이터를 가져옴
+    const listing = await prisma.listing.findUnique({
+      where: {
+        id: listingId
+      },
+      include: {
+        user: true
+      }
+    })
+
+    if (!listing) {
+      return null;
+    }
+
+    // Safe 타입으로 바꾼 listing 데이터 리턴
+    return {
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+      user: {
+        ...listing.user,
+        createdAt: listing.user.createdAt.toISOString(),
+        updatedAt: listing.user.updatedAt.toISOString(),
+        emailVerified: listing.user.emailVerified?.toISOString() || null,
+      }
+    }
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
