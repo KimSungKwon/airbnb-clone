36757bc6a68b590a0a8cd6f4173898616f21ecd5
KimSungKwon <sungkwonn@naver.com>
2023-05-21 16:57:40 +0900

    create getReservation Action

---
+++ airbnb-video/app/actions/getReservations.ts
@@ -0,0 +1,53 @@
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  listingId?: string;
+  userId?: string;
+  authorId?: string;
+}
+
+export default async function getReservations(params: IParams) {
+  try {
+    const { listingId, userId, authorId } = params;
+
+    // 1. params의 데이터로 쿼리 만듬 (셋 중 하나만 얻어도)
+    const query: any = {};
+
+    if (listingId) {
+      query.listingId = listingId;
+    }
+    if (userId) {
+      query.userId = userId;
+    }
+    if (authorId) {
+      query.listing = { userId: authorId };
+    }
+    
+    // 2. 쿼리에 해당하는 reservation들 가져옴
+    const reservations = await prisma.reservation.findMany({
+      where: query,
+      include: {
+        listing: true
+      },
+      orderBy: {
+        createdAt: 'desc'
+      }
+    });
+
+    // 3. 타입 변형
+    const safeReservations = reservations.map((reservation) => ({
+      ...reservation,
+      createdAt: reservation.createdAt.toISOString(),
+      startDate: reservation.startDate.toISOString(),
+      endDate: reservation.endDate.toISOString(),
+      listing: {
+        ...reservation.listing,
+        createdAt: reservation.createdAt.toISOString()
+      }
+    }))
+
+    return safeReservations;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
